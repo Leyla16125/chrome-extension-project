1268a013fda395604dc240b0896295e3c3c61f25
Garib Guluzada <154353203+iskandarli@users.noreply.github.com>
2024-12-01 15:46:17 +0400

    Final Changes

--- get_content_skills.js
+++ get_content_skills.js
@@ -1,18 +1,11 @@
 (function () {
-    function getSkills() {
-        const skills = [];
-        document.querySelectorAll('div.display-flex.align-items-center.mr1.hoverable-link-text.t-bold').forEach(skill => {
-            const skillText = skill.querySelector('span[aria-hidden="true"]');
-            if (skillText) {
-                skills.push(skillText.textContent.trim());
-            }
-        });
-        return skills;
-    }
-
-    const skillsData = {
-        skills: getSkills()
+    const extractSkills = () => {
+        return Array.from(document.querySelectorAll('div.display-flex.align-items-center.mr1.hoverable-link-text.t-bold'))
+            .map((skillContainer) => skillContainer.querySelector('span[aria-hidden="true"]')?.textContent.trim())
+            .filter(Boolean);
     };
 
-    chrome.runtime.sendMessage({ type: 'profileData', data: skillsData });
+    const skills = extractSkills();
+
+    chrome.runtime.sendMessage({ type: 'profileData', data: { skills } });
 })();
